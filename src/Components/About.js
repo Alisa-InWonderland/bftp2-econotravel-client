0a0b4cf7439d11d2d9446aaacfe803206646b7d4
CrypticiaN <obscura.darkside@gmail.com>
2022-02-08 15:34:23 +0100

    react router: mostrar Quiénes somos en página principal, falta esconder el resto

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,1 +1,20 @@
 import React from "react";
+
+export const About = () => {
+    return (
+        <div className="about-container">
+            <header>Econotravel: Experiencias en el área de Barcelona</header>
+            <h1>- Quiénes somos -</h1>
+
+            <h2>Misión</h2>
+            <p>Nuestra empresa se fundó a raíz de un viaje que nuestra fundadora, Marina Herrán, realizó en bicicleta por Cataluña en el verano de 2001. Durante ese periodo, descubrió que las opciones turísticas que ofrecían ciudades como Barcelona, Girona y Reus dependían de transportes contaminantes y actividades poco respetuosas con la naturaleza y el entorno, algo que causaba un progresivo deterioro de los increíbles parajes naturales de Cataluña.</p>
+
+            <p>Preocupada por la falta de alternativas eco-friendly, Marina se propuso fundar una agencia de experiencias turísticas donde el respeto por la naturaleza fuera la principal prioridad. Así nació Econotravel Barcelona.</p>
+
+            <div>
+                <img/>
+                <span>Marina Herrán, fundadora de Econotravel</span>
+            </div>
+        </div>
+    );
+}
