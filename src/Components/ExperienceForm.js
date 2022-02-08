381a949252db085a7bd5af6a0fffd465f09babb7
Desi y Lucas <desimorenoh@gmail.com>
2022-02-08 14:35:26 +0100

    ECON-Modificación de Formulario para editar

--- src/Components/ExperienceForm.js
+++ src/Components/ExperienceForm.js
@@ -3,19 +3,19 @@ import React, {Fragment, useState, useEffect} from 'react';
 
 export const ExperienceForm = (props) => {
 
-    const [datos, setDatos] = useState({
+    const [datos, setDatos] = useState(props.edit ? props.edit.value : {
         name: '',
         description: '',
-        price:'',
-        duration:'',
-        accessibility:'',
-        tags:''
+        price: null,
+        duration: '',
+        accessibility: '',
+        tags: ''
     })
 
     const handleInputChange = (event) => {
         setDatos({
             ...datos,
-            [event.target.name] : event.target.value
+            [event.target.name]: event.target.value
         })
     }
     const enviarDatos = (event) => {
@@ -29,66 +29,135 @@ export const ExperienceForm = (props) => {
         <Fragment>
             <h1>Añadir Nueva Experiencia</h1>
             <form className="row edit-form" onSubmit={enviarDatos}>
+                {props.edit ? (
+                    <>
+                        <div className="col-md-3">
+                            <input type="text"
+                                   placeholder="Título"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="name"
+                                   value={datos.name}/>
+                        </div>
 
-                <div className="col-md-3">
-                    <input type="text"
-                           placeholder="Título"
-                           className="form-control"
-                           onChange={handleInputChange}
-                           name="name"/>
-                </div>
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Descripción"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="description"
+                                   value={datos.description}/>
+                        </div>
 
-                <div className="form-group">
-                    <input type="text"
-                           placeholder="Descripción"
-                           className="form-control"
-                           onChange={handleInputChange}
-                           name="description"/>
-                </div>
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Precio p/p"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="price"
+                                   value={datos.price}/>
+                        </div>
 
-                <div className="form-group">
-                    <input type="text"
-                           placeholder="Precio p/p"
-                           className="form-control"
-                           onChange={handleInputChange}
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Duración"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="duration"
+                                   value={datos.duration}/>
+                        </div>
 
-                           name="price"/>
-                </div>
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Accesibilidad"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="accessibility"
+                                   value={datos.accessibility}/>
+                        </div>
 
-                <div className="form-group">
-                    <input type="text"
-                           placeholder="Duración"
-                           className="form-control"
-                           onChange={handleInputChange}
-
-                           name="duration"/>
-                </div>
-
-                <div className="form-group">
-                    <input type="text"
-                           placeholder="Accesibilidad"
-                           className="form-control"
-                           onChange={handleInputChange}
-
-                           name="accessibility"/>
-                </div>
-
-                <div className="form-group">
-                    <input type="text"
-                           placeholder="Etiquetas"
-                           className="form-control"
-                           onChange={handleInputChange}
-
-                           name="tags"/>
-                </div>
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Etiquetas"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   value={datos.tags}
+                                   name="tags"/>
+                        </div>
 
 
-                <div className="card-footer mb-5">
-                    <input type="submit" className="boton-edit" value="Guardar"/>
-                </div>
-            </form>
+                        <div className="card-footer mb-5">
+                            <input type="submit" className="boton-edit" value="Guardar"/>
+                        </div>
+
+                    </>
+                ) : (
+                    <>
+                        <div className="col-md-3">
+                            <input type="text"
+                                   placeholder="Título"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="name"
+                                   value={datos.name}/>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Descripción"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="description"
+                                   value={datos.description}/>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Precio p/p"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="price"
+                                   value={datos.price}/>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Duración"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="duration"
+                                   value={datos.duration}/>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Accesibilidad"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="accessibility"
+                                   value={datos.accessibility}/>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="text"
+                                   placeholder="Etiquetas"
+                                   className="form-control"
+                                   onChange={handleInputChange}
+                                   name="tags"
+                                   value={datos.tags}/>
+                        </div>
+
+
+                        <div className="card-footer mb-5">
+                            <input type="submit" className="boton-edit" value="Añadir Nuevo"/>
+                        </div>
+
+                    </>
+
+                )}
+            < /form>
         </Fragment>
-            );
+    );
 }
 
 
