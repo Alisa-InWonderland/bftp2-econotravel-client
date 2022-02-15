6ac927e71d7c48e28406b1e798d6cf62999068d4
CrypticiaN <obscura.darkside@gmail.com>
2022-02-15 16:02:37 +0100

    estilos About

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,19 +1,30 @@
 import React from "react";
+import fundadora from "../assets/fundadora.png";
 
 export const About = () => {
     return (
         <div className="about-container">
-            <header>Econotravel: Experiencias en el área de Barcelona</header>
-            <h1>- Quiénes somos -</h1>
+            <div className="about-card">
+                <header className="statement">Econotravel: Experiencias en el área de Barcelona</header>
+                <h1 className="title">- Quiénes somos -</h1>
 
-            <h2>Misión</h2>
-            <p>Nuestra empresa se fundó a raíz de un viaje que nuestra fundadora, Marina Herrán, realizó en bicicleta por Cataluña en el verano de 2001. Durante ese periodo, descubrió que las opciones turísticas que ofrecían ciudades como Barcelona, Girona y Reus dependían de transportes contaminantes y actividades poco respetuosas con la naturaleza y el entorno, algo que causaba un progresivo deterioro de los increíbles parajes naturales de Cataluña.</p>
+                <h2 className="title-mission">Misión</h2>
+                <p className="mission-text">Nuestra empresa se fundó a raíz de un viaje que nuestra fundadora, Marina
+                    Herrán, realizó en bicicleta por Cataluña en el verano de 2001. Durante ese periodo, descubrió que
+                    las opciones turísticas que ofrecían ciudades como Barcelona, Girona y Reus dependían de transportes
+                    contaminantes y actividades poco respetuosas con la naturaleza y el entorno, algo que causaba un
+                    progresivo deterioro de los increíbles parajes naturales de Cataluña.</p>
+                <br/>
+                <p className="mission-text" id="paragr-2">Preocupada por la falta de alternativas eco-friendly, Marina se propuso
+                    fundar una agencia de
+                    experiencias turísticas donde el respeto por la naturaleza fuera la principal prioridad. Así nació
+                    Econotravel Barcelona.</p>
 
-            <p>Preocupada por la falta de alternativas eco-friendly, Marina se propuso fundar una agencia de experiencias turísticas donde el respeto por la naturaleza fuera la principal prioridad. Así nació Econotravel Barcelona.</p>
-
-            <div>
-                <img/>
-                <span>Marina Herrán, fundadora de Econotravel</span>
+                <div>
+                    <img className="fundadora-img" src={fundadora} alt={"Portrait de la fundadora"}/>
+                    <br/>
+                    <span className="fundadora-name">Marina Herrán, fundadora de Econotravel</span>
+                </div>
             </div>
         </div>
     );
