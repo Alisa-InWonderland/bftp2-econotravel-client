bf98a9e2e043ffefd4d419eb1f4734d57340a098
CrypticiaN <obscura.darkside@gmail.com>
2022-02-23 09:13:23 +0100

    añadir HTML de ExperienceDetails

--- src/Components/ExperienceDetails/ExperienceDetails.js
+++ src/Components/ExperienceDetails/ExperienceDetails.js
@@ -4,37 +4,39 @@ import {Link} from "react-router-dom";
 export const ExperienceDetails = () => {
 
     return (
-        <div className="details-container">
-            <div>
-                <img src="#" className="image" alt="Image of Experience"/>
+        <div className="details-card">
+            <div className="details-img-container">
+                <img className="details-img" src="" alt="Imagen Experiencia" />
+                <span className="price">200 €</span>
             </div>
-
-            <table className="experience-table">
-                <tbody>
-
-                <tr>
-                    <td>experience.name</td>
-                    <td>experience.price '€'</td>
-                </tr>
-
-                <tr className='duration'>
-                    <td>experience.duration</td>
-                </tr>
-
-
-                <tr className='btn-card-container'>
-                    <td>
-                        <button className="btn-orange editar">
-                            <Link className="btn-orange-link" to="/add">Editar</Link>
-                        </button>
-                    </td>
-                    <td>
-                        <button className="btn-orange borrar"><a className="btn-orange-link">Borrar</a>
-                        </button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+            <h1 className="title">Title of Experience</h1>
+            <p className="description">Description de la experiencia
+                más extensiva, serán uno o
+                dos párrafos... Lorem ipsum lorem ipsum escription de la experiencia
+                más extensiva, serán uno o
+                dos párrafos... Lorem ipsum lorem ipsum</p>
+            <div className="type-container">
+                <div className="e-place">
+                    <span id="place-type">Tipo</span>
+                    <img id="place-icon" src="" alt="place icon" />
+                    <span id="place-name">Montaña</span>
+                </div>
+                <div className="e-duration">
+                    <span id="duration">Duración</span>
+                    <span id="duration-time">4h</span>
+                    <span id="duration-length">larga</span>
+                </div>
+                <div className="e-transport">
+                    <span id="transport">Transporte</span>
+                    <img id="transport-icon" src="" alt="transport icon" />
+                    <span id="transport-name">Bicicleta</span>
+                </div>
+            </div>
+            <div className="a11y-container">
+                <span className="a11y-name">Accessibilidad:</span>
+                <span className="a11y-description"></span>
+            </div>
+            <button className="btn-reservation">Reservar!</button>
         </div>
     )
 }
